e8873026a920f711abbf8bbb241e25facdeb4d84
KeJunMao <kejun1997@gmail.com>
2023-09-19 18:40:45 +0800

    chore: add test

--- test/context.test.ts
+++ test/context.test.ts
@@ -1,5 +1,35 @@
-import { describe } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
+import nn from 'node-notifier'
+import { normalizePath } from 'vite'
+import { Context } from '../src/core/context'
+
+vi.mock('node-notifier', () => {
+  const nn = vi.fn()
+
+  // @ts-expect-error ignore
+  nn.notify = vi.fn()
+  return {
+    default: nn,
+    notify: vi.fn(),
+  }
+})
 
 describe('context', () => {
+  it('base notification', () => {
+    const ctx = new Context()
+    ctx.notify('base')
+    expect(nn.notify).toBeCalled()
+  })
 
+  it('error notification', () => {
+    const ctx = new Context()
+    ctx.error('error')
+    expect(nn.notify).toBeCalled()
+  })
+  it('contentImage', () => {
+    const ctx = new Context({
+      icon: 'nuxt',
+    })
+    expect(normalizePath(ctx.contentImage)).includes('assets/nuxt.svg')
+  })
 })
