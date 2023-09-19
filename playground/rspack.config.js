1da9a6c1b1f71b35d1916647a927285dce345b77
kejun <kejun1997@gmail.com>
2023-09-19 23:40:12 +0800

    chore: add rspack config in playground

---
+++ playground/rspack.config.js
@@ -0,0 +1,17 @@
+const path = require('node:path')
+const Unplugin = require('unplugin-notifier/rspack').default
+
+/** @typedef {import('@rspack/core').Configuration} RspackConfig */
+/** @type {RspackConfig} */
+const config = {
+  entry: {
+    main: './main.ts',
+  },
+  plugins: [Unplugin()],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
+
+module.exports = config
