516577f2ced43bb41262d15bd0dddbd03e8b92b6
KeJunMao <kejun1997@gmail.com>
2023-09-20 16:23:17 +0800

    fix: apply patch error

--- apply-patch.mjs
+++ apply-patch.mjs
@@ -1,20 +1,26 @@
-import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 import { applyPatchToDir } from '@pnpm/patching.apply-patch'
 
+const _require = createRequire(import.meta.url)
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const moduleName = 'node-notifier'
 const moduleVersion = '10.0.1'
-
-try {
+function apply() {
   applyPatchToDir({
-    patchedDir: join(__dirname, `node_modules/${moduleName}`),
+    patchedDir: dirname(_require.resolve(moduleName)),
     patchFilePath: join(__dirname, `patches/${moduleName}@${moduleVersion}.patch`),
   })
 
   // eslint-disable-next-line no-console
   console.log(`✔ Patched ${moduleName}@${moduleVersion}.`)
 }
+
+try {
+  apply()
+}
 catch (error) {
+  console.error(error)
   console.error(`Unrecognized patch file in patches directory ${moduleName}@${moduleVersion}.patch`)
 }
